9b1bc7522a15f94ae698cdcd9e4600ad5839eb62
vanilla <vanillaiice@tutanota.com>
2024-01-13 21:24:47 +0300

    feat: add support for parsing csv files

---
+++ parser/pcsv/pcsv.go
@@ -0,0 +1,129 @@
+package pcsv
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+	"time"
+
+	"github.com/vanillaiice/verano/activity"
+	"github.com/vanillaiice/verano/db"
+	"github.com/vanillaiice/verano/util"
+)
+
+// ExportToDb populates the database with activities in csv format
+func ExportToDb(b []byte, sqldb *sql.DB) (err error) {
+	var activities []*activity.Activity
+	r := bytes.NewReader(b)
+	reader := csv.NewReader(r)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return
+	}
+
+	for _, record := range records {
+		if record[0] == "Id" {
+			continue
+		}
+		activity, err := recordToActivity(record)
+		if err != nil {
+			return err
+		}
+		activities = append(activities, activity)
+	}
+
+	err = db.InsertActivities(sqldb, activities)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// ActivitiesToCSV converts a slice of activities to csv format
+func ActivitiesToCSV(activities []*activity.Activity, w io.Writer) (err error) {
+	var records [][]string
+	records = append(records, []string{"Id", "Description", "Duration", "Start", "Finish", "PredecessorsId", "SuccessorsId", "Cost"})
+	for _, act := range activities {
+		records = append(records, activityToRecord(act))
+	}
+
+	writer := csv.NewWriter(w)
+	defer writer.Flush()
+
+	err = writer.WriteAll(records)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// Convert record to *Activity pointer
+func recordToActivity(record []string) (act *activity.Activity, err error) {
+	id, err := strconv.Atoi(record[0])
+	if err != nil {
+		return
+	}
+
+	duration, err := time.ParseDuration(record[2])
+	if err != nil {
+		return
+	}
+
+	startTimeInt64, err := strconv.ParseInt(record[3], 10, 64)
+	if err != nil {
+		return
+	}
+	startTime := time.Unix(startTimeInt64, 0)
+
+	finishTimeInt64, err := strconv.ParseInt(record[4], 10, 64)
+	if err != nil {
+		return
+	}
+	finishTime := time.Unix(finishTimeInt64, 0)
+
+	predecessors, err := util.Unflat(record[5])
+	if err != nil {
+		return
+	}
+
+	successors, err := util.Unflat(record[6])
+	if err != nil {
+		return
+	}
+
+	cost, err := strconv.ParseFloat(record[7], 64)
+	if err != nil {
+		return
+	}
+
+	act = &activity.Activity{
+		Id:             id,
+		Description:    record[1],
+		Duration:       duration,
+		Start:          startTime,
+		Finish:         finishTime,
+		PredecessorsId: predecessors,
+		SuccessorsId:   successors,
+		Cost:           cost,
+	}
+
+	return act, nil
+}
+
+// Convert Activity struct to slice of strings
+func activityToRecord(act *activity.Activity) []string {
+	return []string{
+		fmt.Sprint(act.Id),
+		act.Description,
+		act.Duration.String(),
+		fmt.Sprint(act.Start.Unix()),
+		fmt.Sprint(act.Finish.Unix()),
+		util.Flat(act.PredecessorsId),
+		util.Flat(act.SuccessorsId),
+		fmt.Sprint(act.Cost),
+	}
+}
---
+++ parser/pcsv/pcsv_test.go
@@ -0,0 +1,56 @@
+package pcsv
+
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/vanillaiice/verano/activity"
+	"github.com/vanillaiice/verano/db"
+)
+
+var scsv = `Id,Description,Duration,Start,Finish,PredecessorsId,SuccessorsId,Cost
+3,Cook eggs,10m0s,-62135596800,-62135596800,2,1,0
+2,Buy eggs,30m0s,-62135596800,-62135596800,,3,100
+1,Eat eggs,20m0s,-62135596800,-62135596800,3,,0
+`
+var d1 = time.Minute * 10
+var d2 = time.Minute * 30
+var d3 = time.Minute * 20
+var tt = time.Time{}
+var activities = []*activity.Activity{
+	{Id: 3, Description: "Cook eggs", Duration: d1, PredecessorsId: []int{2}, SuccessorsId: []int{1}, Start: tt, Finish: tt, Cost: 0},
+	{Id: 2, Description: "Buy eggs", Duration: d2, PredecessorsId: []int{}, SuccessorsId: []int{3}, Start: tt, Finish: tt, Cost: 100},
+	{Id: 1, Description: "Eat eggs", Duration: d3, PredecessorsId: []int{3}, SuccessorsId: []int{}, Start: tt, Finish: tt, Cost: 0},
+}
+
+func TestExportToDb(t *testing.T) {
+	sqldb, err := db.New("test.db")
+	if err != nil {
+		t.Error(err)
+	}
+	err = ExportToDb([]byte(scsv), sqldb.DB)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Remove("test.db")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestActivitiesToCSV(t *testing.T) {
+	var buf bytes.Buffer
+	w := bufio.NewWriter(&buf)
+	err := ActivitiesToCSV(activities, w)
+	if err != nil {
+		t.Error(err)
+	}
+	sb := buf.String()
+	if sb != scsv {
+		t.Errorf("error parsing csv: want %s, got %s\n", scsv, sb)
+	}
+}
