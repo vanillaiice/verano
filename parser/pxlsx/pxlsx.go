97eb9b70230f3bcbb02bf4cd86bf08f4a5284c05
vanilla <vanillaiice@tutanota.com>
2024-01-15 18:50:56 +0300

    feat: add support for parsing xlsx files

---
+++ parser/pxlsx/pxlsx.go
@@ -0,0 +1,137 @@
+package pxlsx
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/tealeg/xlsx/v3"
+	"github.com/vanillaiice/verano/activity"
+	"github.com/vanillaiice/verano/db"
+	"github.com/vanillaiice/verano/util"
+)
+
+// ExportToDb populates the database with activities in xlsx format
+func ExportToDb(sheet *xlsx.Sheet, sqldb *sql.DB) (err error) {
+	activities, err := XLSXToActivities(sheet)
+	if err != nil {
+		return
+	}
+
+	err = db.InsertActivities(sqldb, activities)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// ActivitiesToXLSX converts a slice of activities to xlsx format
+func ActivitiesToXLSX(activities []*activity.Activity, sheet *xlsx.Sheet) {
+	for _, activity := range activities {
+		row := sheet.AddRow()
+		cells := []*xlsx.Cell{}
+
+		id := row.AddCell()
+		id.SetInt(activity.Id)
+		cells = append(cells, id)
+
+		description := row.AddCell()
+		description.SetString(activity.Description)
+		cells = append(cells, description)
+
+		duration := row.AddCell()
+		duration.SetString(activity.Duration.String())
+		cells = append(cells, duration)
+
+		start := row.AddCell()
+		start.SetDateTime(activity.Start)
+		cells = append(cells, start)
+
+		finish := row.AddCell()
+		finish.SetDateTime(activity.Finish)
+		cells = append(cells, finish)
+
+		predecessorsId := row.AddCell()
+		predecessorsId.SetString(util.Flat(activity.PredecessorsId))
+		cells = append(cells, predecessorsId)
+
+		successorsId := row.AddCell()
+		successorsId.SetString(util.Flat(activity.SuccessorsId))
+		cells = append(cells, successorsId)
+
+		cost := row.AddCell()
+		cost.SetFloat(activity.Cost)
+		cells = append(cells, cost)
+
+		for _, c := range cells {
+			row.PushCell(c)
+		}
+	}
+}
+
+// XLSXToActivities converts activities in xlsx format to a slice of activities
+func XLSXToActivities(sheet *xlsx.Sheet) (activities []*activity.Activity, err error) {
+	for i := 0; i < sheet.MaxRow; i++ {
+		row, err := sheet.Row(i)
+		if err != nil {
+			return activities, err
+		}
+
+		if row.GetCell(0).String() == "Id" {
+			continue
+		}
+		act := &activity.Activity{}
+
+		id, err := row.GetCell(0).Int()
+		if err != nil {
+			return activities, err
+		}
+		description := row.GetCell(1).String()
+		duration, err := time.ParseDuration(row.GetCell(2).String())
+		if err != nil {
+			return activities, err
+		}
+		start := row.GetCell(3)
+		if start.String() == "" || start.String() == "0" {
+			act.Start = time.Time{}
+		} else {
+			startTime, err := start.GetTime(false)
+			if err != nil {
+				return activities, err
+			}
+			act.Start = startTime
+		}
+		finish := row.GetCell(4)
+		if finish.String() == "" || finish.String() == "0" {
+			act.Finish = time.Time{}
+		} else {
+			finishTime, err := finish.GetTime(false)
+			if err != nil {
+				return activities, err
+			}
+			act.Finish = finishTime
+		}
+		predecessorsId, err := util.Unflat(row.GetCell(5).String())
+		if err != nil {
+			return activities, err
+		}
+		successorsId, err := util.Unflat(row.GetCell(6).String())
+		if err != nil {
+			return activities, err
+		}
+		cost, err := row.GetCell(7).Float()
+		if err != nil {
+			return activities, err
+		}
+
+		act.Id = id
+		act.Description = description
+		act.Duration = duration
+		act.PredecessorsId = predecessorsId
+		act.SuccessorsId = successorsId
+		act.Cost = cost
+
+		activities = append(activities, act)
+	}
+
+	return
+}
---
+++ parser/pxlsx/pxlsx_test.go
@@ -0,0 +1,79 @@
+package pxlsx
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/tealeg/xlsx/v3"
+	"github.com/vanillaiice/verano/activity"
+	"github.com/vanillaiice/verano/db"
+)
+
+var d1 = time.Minute * 10
+var d2 = time.Minute * 30
+var d3 = time.Minute * 20
+var tt = time.Time{}
+var activities = []*activity.Activity{
+	{Id: 3, Description: "Cook eggs", Duration: d1, PredecessorsId: []int{2}, SuccessorsId: []int{1}, Start: tt, Finish: tt, Cost: 0},
+	{Id: 2, Description: "Buy eggs", Duration: d2, PredecessorsId: []int{}, SuccessorsId: []int{3}, Start: tt, Finish: tt, Cost: 100},
+	{Id: 1, Description: "Eat eggs", Duration: d3, PredecessorsId: []int{3}, SuccessorsId: []int{}, Start: tt, Finish: tt, Cost: 0},
+}
+
+func TestActivitiesToXLSX(t *testing.T) {
+	wb := xlsx.NewFile()
+	sheet, err := wb.AddSheet("test")
+	if err != nil {
+		t.Error(err)
+	}
+	defer sheet.Close()
+	ActivitiesToXLSX(activities, sheet)
+	err = wb.Save("test.xlsx")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestXLSXtoActivities(t *testing.T) {
+	wb, err := xlsx.OpenFile("test.xlsx")
+	if err != nil {
+		t.Error(err)
+	}
+	acts, err := XLSXToActivities(wb.Sheet["test"])
+	if err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < len(activities); i++ {
+		if acts[i].Id != activities[i].Id {
+			t.Errorf("got %+v, want %+v", acts[i], activities[i])
+		}
+	}
+}
+
+func TestExportToDb(t *testing.T) {
+	sqldb, err := db.New("test.db")
+	if err != nil {
+		t.Error(err)
+	}
+	wb, err := xlsx.OpenFile("test.xlsx")
+	if err != nil {
+		t.Error(err)
+	}
+	sheet := wb.Sheet["test"]
+	if err != nil {
+		t.Error(err)
+	}
+	err = ExportToDb(sheet, sqldb.DB)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Remove("test.db")
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.Remove("test.xlsx")
+	if err != nil {
+		t.Error(err)
+	}
+}
